56e17a59a5c4d26cb2ad3a4499edd73ab74a6670
arnav0608 <207018438+arnav0608@users.noreply.github.com>
2025-08-01 15:30:30 +0530

    updating js

---
+++ app.js
@@ -0,0 +1,9 @@
+const apiKey = '5927d55cfce42e032c14c108b600c903';
+const cityInput = document.getElementById('cityInput');
+const searchBtn = document.getElementById('searchBtn');
+const locationBtn = document.getElementById('locationBtn');
+
+const outputDiv = document.getElementById('output');
+const currentWeatherDiv = document.getElementById('currentWeather');
+const forecastSection = document.getElementById('forecast');
+const forecastCards = document.getElementById('forecastCards');
